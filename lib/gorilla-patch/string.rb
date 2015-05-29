ce9a326c4d474f93acab2a91e907959d2caf6267
Alexander Popov <alex.wayfer@gmail.com>
2015-05-29 16:17:14 +0300

    Add String refine

---
+++ lib/gorilla-patch/string.rb
@@ -0,0 +1,19 @@
+module GorillaPatch
+	module StringExt
+
+		refine String do
+
+			def underscore
+				word = self.dup
+				word.gsub!(/::/, '/')
+				word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+				word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+				word.tr!("-", "_")
+				word.downcase!
+				word
+			end
+
+		end
+
+	end
+end

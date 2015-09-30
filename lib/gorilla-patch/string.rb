ab186e7541c6b33dc13ddf389c5f9a83a748a525
Alexander Popov <alex.wayfer@gmail.com>
2015-09-30 20:00:22 +0300

    Change String.underscore

--- lib/gorilla-patch/string.rb
+++ lib/gorilla-patch/string.rb
@@ -4,13 +4,11 @@ module GorillaPatch
 		refine String do
 
 			def underscore
-				word = self.dup
-				word.gsub!(/::/, '/')
-				word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-				word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
-				word.tr!("-", "_")
-				word.downcase!
-				word
+				self.gsub!(/::/, '/')
+				  .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+				  .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+				  .tr('-', '_')
+				  .downcase
 			end
 
 		end

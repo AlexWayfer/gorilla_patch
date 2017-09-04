11cfb3c4fb2a0e01c57c83d9c24ba1c8e7670a79
Alexander Popov <alex.wayfer@gmail.com>
2017-09-04 18:28:09 +0300

    Cover `Hash#deep_merge` method with block, reach 100% test covering

--- spec/gorilla-patch/deep_merge_spec.rb
+++ spec/gorilla-patch/deep_merge_spec.rb
@@ -16,6 +16,19 @@ describe GorillaPatch::DeepMerge do
 			it { expect(subject).not_to be init_hash }
 			it { expect(subject[:b]).to be init_hash[:b] }
 			it { expect(subject[:c][:e]).to be merged_hash[:c][:e] }
+
+			context 'with block' do
+				let(:init_hash)   { { a: 100, b: 200, c: { c1: 100 } } }
+				let(:merged_hash) { {         b: 250, c: { c1: 200 } } }
+
+				subject do
+					init_hash.deep_merge(merged_hash) do |_key, this_val, other_val|
+						this_val + other_val
+					end
+				end
+
+				it { expect(subject).to eq(a: 100, b: 450, c: { c1: 300 }) }
+			end
 		end
 
 		describe '#deep_merge!' do

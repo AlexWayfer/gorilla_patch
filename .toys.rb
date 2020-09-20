ee87cf57f658d563175c1e56154a994338f4334b
Alexander Popov <alex.wayfer@gmail.com>
2020-09-21 00:54:45 +0300

    Drop `rspec` and `rubocop` toys, just use `bundle exec`

--- .toys.rb
+++ .toys.rb
@@ -2,36 +2,7 @@
 
 include :bundler, static: true
 
-subtool_apply do
-	include :exec, exit_on_nonzero_status: true, log_level: Logger::UNKNOWN unless include? :exec
-end
-
 require 'gem_toys'
 expand GemToys::Template
 
 alias_tool :g, :gem
-
-template 'shell command' do
-	on_expand do
-		disable_argument_parsing
-	end
-end
-
-tool :rspec do
-	expand 'shell command'
-
-	def run
-		exec ['rspec', *args]
-	end
-end
-
-alias_tool :spec, :rspec
-alias_tool :test, :rspec
-
-tool :rubocop do
-	expand 'shell command'
-
-	def run
-		exec ['rubocop', *args]
-	end
-end

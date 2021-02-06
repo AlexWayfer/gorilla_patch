6d92e866a92bb801235e3a1e88a64764aeba9e33
Alexander Popov <alex.wayfer@gmail.com>
2021-02-06 03:33:22 +0300

    Improve implementation of `GorillaPatch::Cover`
    Don't define refined method at all for Ruby >= 2.6.
    
    Thanks to @zverok: https://bugs.ruby-lang.org/issues/15504#note-15
    
    But it still doesn't resolve an issue with frozen Range in specs.

--- lib/gorilla_patch/cover.rb
+++ lib/gorilla_patch/cover.rb
@@ -4,10 +4,12 @@ module GorillaPatch
 	## Adding include methods
 	module Cover
 		refine Range do
-			def cover?(value)
-				return super unless value.is_a?(Range) && RUBY_VERSION < '2.6'
+			if RUBY_VERSION < '2.6'
+				def cover?(value)
+					return super unless value.is_a?(Range)
 
-				super(value.first) && super(value.last)
+					super(value.first) && super(value.last)
+				end
 			end
 		end
 	end

b2a39988af69194cf8b1660b33b32a8b019b77a6
Alexander Popov <alex.wayfer@gmail.com>
2024-12-31 12:30:49 +0300

    Improve specs for `dry_inflector`

--- spec/gorilla_patch/inflections/dry_inflector_spec.rb
+++ spec/gorilla_patch/inflections/dry_inflector_spec.rb
@@ -6,13 +6,13 @@ describe 'GorillaPatch::Inflections.from_dry_inflector' do
 	describe String do
 		let(:string) { 'GorillaPatch' }
 
-		it { expect(string.camelize).to be_truthy }
-		it { expect(string.constantize).to be_truthy }
-		it { expect(string.dasherize).to be_truthy }
-		it { expect(string.demodulize).to be_truthy }
-		it { expect(string.humanize).to be_truthy }
-		it { expect(string.pluralize).to be_truthy }
-		it { expect(string.singularize).to be_truthy }
-		it { expect(string.underscore).to be_truthy }
+		it { expect(string.camelize).to eq 'GorillaPatch' }
+		it { expect(string.constantize).to eq GorillaPatch }
+		it { expect(string.dasherize).to eq 'GorillaPatch' }
+		it { expect(string.demodulize).to eq 'GorillaPatch' }
+		it { expect(string.humanize).to eq 'Gorillapatch' }
+		it { expect(string.pluralize).to eq 'GorillaPatches' }
+		it { expect(string.singularize).to eq 'GorillaPatch' }
+		it { expect(string.underscore).to eq 'gorilla_patch' }
 	end
 end
